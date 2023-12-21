dfcdcd92c9a61171ff01c29b07c6653c67d8495d
Poyoman <Poyoman39@users.noreply.github.com>
2023-12-21 10:59:11 +0100

    Better defaults

--- src/reporters/FancyReporter.js
+++ src/reporters/FancyReporter.js
@@ -31,7 +31,6 @@ const LEVEL_IS_BADGE = {
   fatal: true,
   error: true,
   warn: true,
-  info: true,
 };
 
 const getBgColor = (color = 'white') => (
@@ -39,7 +38,11 @@ const getBgColor = (color = 'white') => (
   || colors.bgWhite
 );
 
-const DEFAULT_FORMAT_DATE = (date) => `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+const DEFAULT_FORMAT_DATE = (date) => [
+  `${date.getHours()}`.padStart(2, '0'),
+  `${date.getMinutes()}`.padStart(2, '0'),
+  `${date.getSeconds()}`.padStart(2, '0'),
+].join(':');
 
 function FancyReporter(options = {}) {
   // === Read options === //
